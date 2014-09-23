5417e30e37d15e698eb364e56717648abd876076
Sebastian Karasiewicz <sekarasiewicz@gmail.com>
2014-09-23 15:04:57 +0200

    Add new action and form, add strong params strategy

--- app/controllers/servants_controller.rb
+++ app/controllers/servants_controller.rb
@@ -1,4 +1,18 @@
 class ServantsController < ApplicationController
   expose(:servants)
-  expose(:servant)
+  expose(:servant, attributes: :servant_params)
+
+  def create
+    if servant.save
+      redirect_to(servant)
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def servant_params
+    params.require(:servant).permit!
+  end
 end
