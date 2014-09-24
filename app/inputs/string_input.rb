3a5c8a2e60078ed857eab16c1ae672ffed10a7b6
Sebastian Karasiewicz <sekarasiewicz@gmail.com>
2014-09-24 18:56:13 +0200

    reformat select class, add string class to inputs

--- app/inputs/string_input.rb
+++ app/inputs/string_input.rb
@@ -1,13 +1,17 @@
-class SelectInput
+class StringInput
 
   include Formtastic::Inputs::Base
-  include Formtastic::Inputs::Base::Collections
 
   def to_html
+
     template.content_tag(:div, class: 'form-group') do
       label_html <<
           select_html
     end
+  end
+
+  def select_html
+    builder.text_field(method, input_html_options)
   end
 
   def label_html_options
@@ -15,10 +19,6 @@ class SelectInput
         for: input_html_options[:id],
         class: ['control-label'],
     }
-  end
-
-  def select_html
-    builder.select(input_name, collection, input_options, input_html_options)
   end
 
   def input_html_options
