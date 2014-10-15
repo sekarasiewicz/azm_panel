de7b4cfe50a40d83d9afbc1d64ff2a3758eb90ae
Sebastian Karasiewicz <sekarasiewicz@gmail.com>
2014-10-15 21:58:54 +0200

    add recommended unicorn configuration

--- config/unicorn/production.rb
+++ config/unicorn/production.rb
@@ -6,12 +6,40 @@ stderr_path "#{root}/log/unicorn.log"
 stdout_path "#{root}/log/unicorn.log"
 
 # Port configuration
-listen 3000
+# listen 3000
 worker_processes 2
-timeout 30
+# timeout 30
 
 # Force the bundler gemfile environment variable to
 # reference the capistrano "current" symlink
 before_exec do |_|
   ENV['BUNDLE_GEMFILE'] = File.join(root, 'Gemfile')
+end
+
+
+preload_app true
+
+before_fork do |server, worker|
+  # the following is highly recomended for Rails + "preload_app true"
+  # as there's no need for the master process to hold a connection
+  if defined?(ActiveRecord::Base)
+    ActiveRecord::Base.connection.disconnect!
+  end
+
+  # Before forking, kill the master process that belongs to the .oldbin PID.
+  # This enables 0 downtime deploys.
+  old_pid = "#{server.config[:pid]}.oldbin"
+  if File.exists?(old_pid) && server.pid != old_pid
+    begin
+      Process.kill('QUIT', File.read(old_pid).to_i)
+    rescue Errno::ENOENT, Errno::ESRCH
+      # someone else did our job for us
+    end
+  end
+end
+
+after_fork do |server, worker|
+  if defined?(ActiveRecord::Base)
+    ActiveRecord::Base.establish_connection
+  end
 end
