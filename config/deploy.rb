4131cc5ddb92b0a023430b07a2af38f186fe4244
Sebastian Karasiewicz <sekarasiewicz@gmail.com>
2014-10-22 17:46:38 +0200

    add secrets.yml to linked deploy files

--- config/deploy.rb
+++ config/deploy.rb
@@ -9,7 +9,7 @@ set :rails_env, 'production'
 set :rvm_ruby_version, '2.1.3@azm_panel'
 set :linked_dirs, %w{bin log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system}
 # Default value for :linked_files is []
-set :linked_files, %w{config/database.yml .env}
+set :linked_files, %w{config/database.yml config/secrets.yml}
 
 # Default value for linked_dirs is []
 # set :linked_dirs, %w{bin log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system}
