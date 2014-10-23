39f12f8aed70c92c84ca7aa7954d001e1ae03e0a
Sebastian Karasiewicz <sekarasiewicz@gmail.com>
2014-10-23 10:10:59 +0200

    add create action

--- app/controllers/rank_controller.rb
+++ app/controllers/rank_controller.rb
@@ -1,4 +1,22 @@
 class RankController < ApplicationController
   expose(:rank_all) { Rank.all }
-  expose(:rank, ancestor: :rank_all)
+  expose(:rank, ancestor: :rank_all, attributes: :rank_params)
+
+  def create
+    if rank.save
+      redirect_to_index
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def rank_params
+    params.require(:rank).permit!
+  end
+
+  def redirect_to_index
+    redirect_to rank_index_path
+  end
 end
